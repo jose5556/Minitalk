6b536562b5ab0ad82f0d8b7de93ad4a4f58a9db1
jose5556 <josefilmeneses@gmail.com>
2024-07-18 12:34:45 +0100

    idk, still wrong

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 19:18:26 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/07/18 08:17:49 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/07/18 12:31:52 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,70 @@
 
 void	server_handler(int signum)
 {
-	printf("cao");
+	if (signum == SIGUSR1)
+	{
+		write (1, "1", 1);
+		write (1, "\n", 1);
+	}
+	else if (signum == SIGUSR2)
+	{
+		write (1, "0", 1);
+		write (1, "\n", 1);
+	}
 }
 
-int		main(void)
+int	main(void)
 {
-	struct sigaction 	sa;
+	struct sigaction	sa;
 	int					pid;
-	
+
 	sa.sa_handler = &server_handler;
 	pid = getpid();
 	ft_putnbr(pid);
-	
-	sigaction(SIGINT, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
+	write (1, "\n", 1);
+	if (sigaction(SIGUSR1, &sa, NULL) == -1)
+		exit(1);
+	if (sigaction(SIGUSR2, &sa, NULL) == -1)
+		exit(1);
+	while (1)
+	{
+		//sleep (1000);
+		pause();
+	}
+}
+
+void	ft_putnbr(int n)
+{
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n *= -1;
+	}
+	if (n > 9)
+		ft_putnbr(n / 10);
+	ft_putchar(n % 10 + 48);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	ft_memset(s, '\0', n);
+}
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	int	i;
+
+	i = 0;
+	while (n)
+	{
+		((char *)s)[i] = c;
+		n--;
+		i++;
+	}
+	return (s);
 }
