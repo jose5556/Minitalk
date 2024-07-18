6727247bdaa4c17a43243f4b44decee1959b5774
jose5556 <josefilmeneses@gmail.com>
2024-07-18 12:33:34 +0100

    added bzero

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 08:08:10 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/07/18 08:25:08 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/07/18 12:31:07 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,13 @@
 # include <stdlib.h>
 # include <signal.h>
 # include <stdio.h>
-# include "./include/ft_printf/ft_printf.h"
-# include "./include/libft/libft.h"
+/* # include "./include/ft_printf/ft_printf.h"
+# include "./include/libft/libft.h" */
 
 void	client_handler(int pid, char *str);
 void	server_handler(int signum);
+void	ft_putchar(char c);
+void	ft_putnbr(int n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
 #endif
