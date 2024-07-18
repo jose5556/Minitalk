28a9f54cfea3fad8502163362bc3bd65fe54720c
jose5556 <josefilmeneses@gmail.com>
2024-07-18 08:18:58 +0100

    funcs added

---
+++ minitalk.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/18 08:08:10 by joseoliv          #+#    #+#             */
+/*   Updated: 2024/07/18 08:17:33 by joseoliv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <stdio.h>
+
+void	client_handler(int pid, char *str);
+void	server_handler(int signum);
+#endif
