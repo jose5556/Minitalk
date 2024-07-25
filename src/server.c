e196f446b20789d58a7e849a38dd6a21a1af65ed
jose5556 <josefilmeneses@gmail.com>
2024-07-25 11:03:02 +0100

    server_handler modified, still error

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 19:18:26 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/07/25 09:33:42 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/07/25 11:00:22 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,18 @@
 
 void	server_handler(int signum)
 {
+	static int i = 0;
+	static char chr = 0;
+
 	if (signum == SIGUSR1)
 	{
-		ft_printf("1");
+		chr = (chr >> i) | 1;
 	}
 	else if (signum == SIGUSR2)
 	{
-		ft_printf("0");
+		chr = (chr >> i) & 1;
 	}
+	ft_printf("%c", chr);
 }
 
 int	main(void)
@@ -30,9 +34,10 @@ int	main(void)
 	int					pid;
 
 	sa.sa_handler = &server_handler;
+	sa.sa_flags = 0;
 	sigemptyset(&sa.sa_mask);
 	pid = getpid();
-	ft_printf("%d\n", pid);
+	ft_printf("Processor ID is: %d\n", pid);
 	if (sigaction(SIGUSR1, &sa, NULL) == -1)
 		exit(1);
 	if (sigaction(SIGUSR2, &sa, NULL) == -1)
