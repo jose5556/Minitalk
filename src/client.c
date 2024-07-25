15cc39337c26130fe416661813bae086c9258f50
jose5556 <josefilmeneses@gmail.com>
2024-07-25 10:32:21 +0100

    added usleep and is working fine

--- src/client.c
+++ src/client.c
@@ -6,40 +6,45 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 19:18:22 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/07/25 09:27:39 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/07/25 10:29:29 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minitalk.h"
 
-void	client_handler(int pid, char *str)
+void	client_handler(int pid, char c)
 {
 	int		i;
 
 	i = 7;
-	while (*str)
+	while (i >= 0)
 	{
-		while (i >= 0)
-		{
-			if ((*str >> i) & 1)
-				kill(pid, SIGUSR1);
-			else
-				kill(pid, SIGUSR2);
-			i--;
-			str++;
-		}
-		str++;
-		i = 7;
+		if ((c >> i) & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		i--;
+		usleep(150);
 	}
 }
 
 int	main(int argc, char *argv[])
 {
-	if (argc == 3)
-		client_handler(ft_atoi(argv[1]), argv[2]);
-	else
+	int	i;
+	int	pid;
+
+	i = 0;
+	if (argc != 3)
 	{
-		ft_printf(" no no no ");
+		ft_printf("no no no, write the PID, and then the message please!");
 		return (1);
+	}
+	pid = ft_atoi(argv[1]);
+	if (pid == -1)
+		return (1);
+	while (argv[2][i])
+	{
+		client_handler(pid, argv[2][i]);		
+		i++;
 	}
 }
