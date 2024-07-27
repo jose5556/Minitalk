8fccde565a66c7c758b8cdbb25968218bea21134
jose5556 <josefilmeneses@gmail.com>
2024-07-27 17:08:06 +0100

    convert correctly binary into words

--- src/server.c
+++ src/server.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 19:18:26 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/07/27 17:03:21 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/07/27 17:06:47 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	server_handler(int signum)
 {
 	static int i = 7;
-	static int decimal_num = 0;
+	static char decimal_num = 0;
 
 	if (signum == SIGUSR1)
 	{
@@ -28,7 +28,7 @@ void	server_handler(int signum)
 		return ;
 	if (i < 0)
 	{
-		ft_printf("%d\n", decimal_num);
+		write (1, &decimal_num, 1);
 		i = 7;
 		decimal_num = 0;
 	}
