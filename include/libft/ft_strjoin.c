6c5379eebf058cc3144dc113e102b4c758080432
jose5556 <josefilmeneses@gmail.com>
2024-08-01 14:57:21 +0100

    changed to s2 be a singular character

--- include/libft/ft_strjoin.c
+++ include/libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 17:34:14 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/07/27 17:58:09 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/08/01 13:56:04 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,15 @@
 char	*ft_strjoin(char const *s1, char const s2)
 {
 	char	*result;
-	int		i;
-	int		j;
+	int		len;
 
 	if (!s1 && !s2)
 		ft_strdup("");
-	result = (char *) malloc((ft_strlen(s1) + ft_strlen(s2) + 1));
+	len = ft_strlen(s1);
+	result = (char *) ft_calloc(len + 2, 1);
 	if (result == NULL)
 		return (result);
-	i = 0;
-	j = 0;
-	while (s1[j])
-	{
-		result[i] = ((char *)s1)[j];
-		i++;
-		j++;
-	}
-	j = 0;
-	while (s2[j])
-		result[i++] = ((char *)s2)[j++];
-	result[i] = 0;
+	ft_strlcpy(result, s1, len + 1);
+	result[len] = s2;
 	return (result);
 }
-
-/* int	main(void)
-{
-	char	s1[4] = "";
-	char	s2[4] = "5";
-
-	printf("%s", ft_strjoin(s1, s2));
-} */
