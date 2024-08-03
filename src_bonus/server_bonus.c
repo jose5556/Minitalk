a97334ee6ffc0285a6bc1814549270b4d7ee409b
jose5556 <josefilmeneses@gmail.com>
2024-08-03 17:49:39 +0100

    sends SIGUSR1 to client

--- src_bonus/server_bonus.c
+++ src_bonus/server_bonus.c
@@ -6,7 +6,7 @@
 /*   By: joseoliv <joseoliv@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 16:32:42 by joseoliv          #+#    #+#             */
-/*   Updated: 2024/08/02 16:34:19 by joseoliv         ###   ########.fr       */
+/*   Updated: 2024/08/03 17:48:21 by joseoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,16 +38,21 @@ void	fill_string(char c)
 	}
 }
 
-void	server_handler(int signum)
+void	server_handler(int signum, siginfo_t *info, void *ucontext)
 {
 	static int	i = 7;
 	static char	decimal_num = 0;
+	int			pid;
 
+	(void)ucontext;
+	pid = info->si_pid;
 	if (signum == SIGUSR1)
 		decimal_num += 1 << i;
 	i--;
 	if (i < 0)
 	{
+		if (decimal_num == 0)
+			kill(pid, SIGUSR1);
 		fill_string(decimal_num);
 		i = 7;
 		decimal_num = 0;
@@ -59,8 +64,8 @@ int	main(void)
 	struct sigaction	sa;
 	int					pid;
 
-	sa.sa_handler = &server_handler;
-	sa.sa_flags = 0;
+	sa.sa_sigaction = server_handler;
+	sa.sa_flags = SA_SIGINFO;
 	sigemptyset(&sa.sa_mask);
 	pid = getpid();
 	ft_printf("Processor ID is: %d\n", pid);
